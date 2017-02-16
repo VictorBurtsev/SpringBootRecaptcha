7a7cb34e3a20dd136de4fedba9d2c57be3dd2099
viktorburcev <victor.burtsev@gmail.com>
2017-02-16 22:04:07 +0300

    main.js- Reformated code

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -1,24 +1,24 @@
 angular.module("reCaptchaDemo", [])
     .controller("AppCtrl", function ($scope, $http) {
         $scope.auth = {};
-        $scope.sendForm = function(auth) {
+        $scope.sendForm = function (auth) {
             $http({
                 method: "POST",
                 url: "/register",
                 data: $.param(auth),
-                headers: { "Content-Type" : "application/x-www-form-urlencoded" }
+                headers: {"Content-Type": "application/x-www-form-urlencoded"}
 
             }).then(
-                function(data) {
+                function (data) {
                     window.alert("Успешно зарегистрирован");
                 },
-                function(error) {
+                function (error) {
                     window.alert("При регистрации произошла ошибка");
                 }
             )
         }
     })
-    .directive("recaptcha", function() {
+    .directive("recaptcha", function () {
         return {
             require: "ngModel",
             restrict: "E",
@@ -26,7 +26,7 @@ angular.module("reCaptchaDemo", [])
                 sitekey: "@",
                 ngModel: "="
             },
-            link: function(scope, element, attrs, ngModelCtrl) {
+            link: function (scope, element, attrs, ngModelCtrl) {
                 var reCaptcha = document.createElement("script");
                 reCaptcha.type = "text/javascript";
                 reCaptcha.async = true;
@@ -34,7 +34,7 @@ angular.module("reCaptchaDemo", [])
                 var firstScript = document.getElementsByTagName("script")[0];
                 firstScript.parentNode.insertBefore(reCaptcha, firstScript);
 
-                window.onLoadReCaptchaCallback = function() {
+                window.onLoadReCaptchaCallback = function () {
                     grecaptcha.render(element.get(0), {
                         "sitekey": scope.sitekey,
                         "callback": onRecaptchaSubmit,
@@ -42,17 +42,17 @@ angular.module("reCaptchaDemo", [])
                     });
                 };
 
-                window.onRecaptchaSubmit = function(gRecaptchaResponse) {
+                window.onRecaptchaSubmit = function (gRecaptchaResponse) {
                     scope.ngModel = gRecaptchaResponse;
                     ngModelCtrl.$setViewValue(gRecaptchaResponse);
                 };
 
-                window.onRecaptchaExpired = function() {
+                window.onRecaptchaExpired = function () {
                     scope.ngModel = "";
                     ngModelCtrl.$setViewValue("");
                 };
 
-                ngModelCtrl.$validators.recaptchaValidate = function(modelValue, viewValue) {
+                ngModelCtrl.$validators.recaptchaValidate = function (modelValue, viewValue) {
                     return !ngModelCtrl.$isEmpty(scope.ngModel);
                 }
             }
